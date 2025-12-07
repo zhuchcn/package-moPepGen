9a5b8dfec5ce002f91c2c8f432a36dc906f8fd3a
zhuchcn <zhuchcn@gmail.com>
2025-12-07 09:34:02 +0800

    fix (util): modules not made accessible

--- moPepGen/util/__init__.py
+++ moPepGen/util/__init__.py
@@ -2,6 +2,16 @@
 
 PROG_NAME = 'moPepGen-util'
 
-# Import brute_force module to maintain backward compatibility
-# This allows: from moPepGen import util; util.brute_force.main()
+# Import all util modules to make them accessible as util.module_name
+# This is required for __main__.py which accesses them as:
+#   util.brute_force.parse_args(subparsers)
+#   util.fuzz_test.parse_args(subparsers)
+#   etc.
 from . import brute_force
+from . import brute_force_novel_orf
+from . import downsample_reference
+from . import validate_variant_calling
+from . import fuzz_test
+from . import extract_gvf
+from . import validate_novel_orf_calling
+from . import add_fuzz_test_log
